93859f3efb5ac7686b5e5e41f2c59db350c1fa87
sujivasagar <sujivasa31@gmail.com>
2024-09-06 22:47:37 -0500

    add the files

--- src/sept/ex_092024/lab03.java
+++ src/sept/ex_092024/lab03.java
@@ -1,4 +1,9 @@
 package sept.ex_092024;
 
-public class lab03 ty6y6y7623{
+public class lab03 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java");
+    }
+
 }
