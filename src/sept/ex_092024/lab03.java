a286865591680e16c4482c2b48c8a4a0ead03c83
sujivasagar <sujivasa31@gmail.com>
2024-09-06 22:55:46 -0500

    add the files

--- src/sept/ex_092024/lab03.java
+++ src/sept/ex_092024/lab03.java
@@ -4,6 +4,7 @@ public class lab03 {
     public static void main(String[] args)
     {
         System.out.println("Hello Java");
+
     }
 
 }
---
+++ src/sept/ex_092025/taskAddition.java
@@ -0,0 +1,15 @@
+package sept.ex_092025;
+
+public class taskAddition {
+    int num = 100, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+    {
+        // sum = sum + i;
+        sum += i;
+    }
+
+        System.out.println("Sum = " + sum);
+}
+}
+}
